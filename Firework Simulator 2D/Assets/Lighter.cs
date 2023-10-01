bbc6b6b26ce49ed6f581ea03374e2f6e346c2c52
Vats <98044806+vats0007@users.noreply.github.com>
2023-10-01 16:18:48 +0530

    Lighter Setup is done

---
+++ Firework Simulator 2D/Assets/Lighter.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighter : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    [SerializeField]
+    private int spriteIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            spriteIndex = 1;
+        }
+        if(Input.touchCount == 0)
+        {
+            spriteIndex = 0;
+        }
+        if (Input.touchCount > 0)
+        {
+
+            Touch touch = Input.GetTouch(0);
+            Vector2 touch_Pos = Camera.main.ScreenToWorldPoint(touch.position);
+            transform.position = touch_Pos + new Vector2(-0.3f,0.5f);
+        }
+
+
+
+
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+
+
+    
+}
